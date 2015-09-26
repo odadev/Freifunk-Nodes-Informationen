aa5733756fc5ed1b071f710f999d5b947c601863
Timo Jeske <timo@jeske.email>
2015-09-26 16:18:58 +0200

    Eine weitere JSON-Datei hinzugefügt, allerdings auskommentiert. Soll nur darauf hinweisen, dass die Links später dynamisch eingebunden werden sollen!

--- config/paths.php
+++ config/paths.php
@@ -1,3 +1,7 @@
 <?php if(!'__IN_SITE__') { die(); }
 
+// Später aus der Datenbank holen
 define('__JSON_NODES_PATH__', 'http://api.freifunk-niersufer.de/mo/nodes.json');
+
+// Andere JSON (Freifunk Krefeld) als Beispiel für die Unabhängigkeit
+//define('__JSON_NODES_PATH__', 'http://api.freifunk-niersufer.de/kr/nodes.json');
