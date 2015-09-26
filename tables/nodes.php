452931c6c4d6a4c29e385609136d520b71be1a63
Timo Jeske <timo@jeske.email>
2015-09-26 16:16:11 +0200

    Kleine Kästchen mit besonderen Informationen entfernt, sodass die Datei nur noch die Tabelle mit den Nodes beinhaltet

--- tables/nodes.php
+++ tables/nodes.php
@@ -1,84 +1,3 @@
-<?php    
-    // Abfrage wie viele Clients zur Zeit online sind und welcher der neuste Node ist
-    $numberOfClients = 0;
-    $newestNode = "";
-    $newestNodeDate = "";
-    $longestUptime = 0;
-    $longestUptimeHostname = 0;
-    
-    $first = true;
-    foreach($nodes as $node) {
-        $numberOfClients += $node->getClients();
-     
-        if($first == true) {
-            $newestNodeDate = $node->getFirstSeen();
-            $newestNode = $node->getHostname();
-            
-            $longestUptime = $node->getUptime();
-            $longestUptimeHostname = $node->getHostname();
-            $first = false;
-        } else {
-            // Überprüfung für neuste Node
-            if(strtotime($node->getFirstSeen()) > strtotime($newestNodeDate)) {
-                $newestNode = $node->getHostname();
-                $newestNodeDate = $node->getFirstSeen();
-            }
-            
-            // Überprüfung für längste Uptime
-            if($node->getUptime() > $longestUptime) {
-                $longestUptime = $node->getUptime();
-                $longestUptimeHostname = $node->getHostname();
-            }
-        }
-    }
-?>
-
-<div class="row">
-    <!-- Anzahl an Nodes -->
-    <div class="col-md-3">
-        <div class="info-box">
-            <span class="info-box-icon bg-fuchsia-active"><i class="fa fa-globe"></i></span>
-            <div class="info-box-content">
-                <span class="info-box-text">Anzahl Nodes</span>
-                <span class="info-box-number"><?php echo sizeof($nodes); ?></span>
-            </div>
-        </div>
-    </div>
-    
-    <!-- Clients online -->
-    <div class="col-md-3">
-        <div class="info-box">
-            <span class="info-box-icon bg-purple"><i class="fa fa-users"></i></span>
-            <div class="info-box-content">
-                <span class="info-box-text">Clients online</span>
-                <span class="info-box-number"><?php echo $numberOfClients; ?></span>
-            </div>
-        </div>
-    </div>
-    
-    <!-- Neuster Node -->
-    <div class="col-md-3">
-        <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="fa fa-plus"></i></span>
-            <div class="info-box-content">
-                <span class="info-box-text">Neuster Node</span>
-                <span class="info-box-number"><?php echo $newestNode; ?><br /><span class="info-box-number-extra"><?php echo $newestNodeDate; ?></span></span>
-            </div>
-        </div>
-    </div>
-    
-    <!-- Am längsten online Node -->
-    <div class="col-md-3">
-        <div class="info-box">
-            <span class="info-box-icon bg-orange"><i class="fa fa-arrow-up"></i></span>
-            <div class="info-box-content">
-                <span class="info-box-text">Am längsten online</span>
-                <span class="info-box-number"><?php echo $longestUptimeHostname; ?><br /><span class="info-box-number-extra"><?php echo number_format($longestUptime / 60 / 60 / 24, 0); ?> Tage</span></span>
-            </div>
-        </div>
-    </div>
-</div>
-
 <div class="row">
     <div class="col-xs-12">
         <div id="nodes-table" class="box box-solid box-freifunk-default">
@@ -86,6 +5,14 @@
                 <i class="fa fa-list-ul"></i><h3 class="box-title">Nodes</h3>
             </div>
             <div class="box-body">
+                <div class="toggle-columns">
+                    Spalte ein-/ausblenden: 
+                    <a class="toggle-vis" data-column="0">Hostname</a> - 
+                    <a class="toggle-vis" data-column="1">Uptime</a> - 
+                    <a class="toggle-vis" data-column="2">Load AVG</a> - 
+                    <a class="toggle-vis" data-column="3">Speicherauslastung</a> - 
+                    <a class="toggle-vis" data-column="4">Clients</a>
+                </div>
                 <table id="nodesTable" class="display responsive nowrap" width="100%">
                     <thead>
                         <tr>
@@ -126,7 +53,7 @@
             "bAutoWidth": true,
             "stateSave": false,
             "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "Alle"]],
-            "order": [[ 2, "desc" ]],
+            "order": [[ 4, "desc" ]],
             "responsive": true,
             "language":
                 {
@@ -153,5 +80,15 @@
                     }
                 }
         });
+        
+        $('a.toggle-vis').on( 'click', function (e) {
+            e.preventDefault();
+
+            // Get the column API object
+            var column = nodesTable.column( $(this).attr('data-column') );
+
+            // Toggle the visibility
+            column.visible( ! column.visible() );
+        } );
     });
 </script>
