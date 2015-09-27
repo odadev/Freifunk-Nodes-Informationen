0a6b007ae70b26461faaf4eadb62e69b4c82f941
Timo Jeske <timo@jeske.email>
2015-09-27 05:03:22 +0200

    Andere JSON-Dateien für andere Communities hinzugefügt (zum Testen), allerdings auskommentiert

--- config/paths.php
+++ config/paths.php
@@ -3,5 +3,8 @@
 // Später aus der Datenbank holen
 define('__JSON_NODES_PATH__', 'http://api.freifunk-niersufer.de/mo/nodes.json');
 
+// Freifunk Rheinland JSON zum Testen
+//define('__JSON_NODES_PATH__', 'http://ffmap.freifunk-rheinland.net/nodes.json');
+
 // Andere JSON (Freifunk Krefeld) als Beispiel für die Unabhängigkeit
 //define('__JSON_NODES_PATH__', 'http://api.freifunk-niersufer.de/kr/nodes.json');
